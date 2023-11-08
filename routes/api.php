0f1b5cc07c07241e6bd6f19937d3c721b0eaa2f2
LAPTOP-RVP0I873\danie <danielgomesalbernaz@gmail.com>
2023-11-08 16:29:42 -0300

    Readme

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/product',[ProductController::class, 'registerProducts']);
+Route::get('/products',[ProductController::class, 'registerProducts']);
